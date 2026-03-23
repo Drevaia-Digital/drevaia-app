53fe98ac6c360073d2ad94c22402da03b0262a99
Noa Drevaia <noadrevaia@gmail.com>
2026-03-22 19:28:45 -0700

    app safe mode real

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,3 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from '@/pages/HomePage';
-import { LibraryPage } from '@/pages/LibraryPage';
-
 export default function App() {
-  const language = "es";
-  const t = (key: string) => key;
-  const changeLanguage = () => {};
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route 
-          path="/" 
-          element={
-            <HomePage 
-              t={t}
-              language={language}
-              changeLanguage={changeLanguage}
-            />
-          } 
-        />
-
-        <Route 
-          path="/library" 
-          element={
-            <LibraryPage 
-              t={t}
-              language={language}
-              changeLanguage={changeLanguage}
-            />
-          } 
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>APP SAFE MODE</h1>;
 }
