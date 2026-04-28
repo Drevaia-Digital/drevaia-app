90bbd103785d5171790c5cecc065594596952c7e
Noa Drevaia <noadrevaia@gmail.com>
2026-04-27 23:47:31 -0500

    mensaje

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -257,7 +257,7 @@ export function Hero({ language }: Props) {
   )}
 
   {/* BOTÓN SECUNDARIO */}
-  <Link to={`/${language}/empieza`}>
+  <Link to="/empieza">
     <Button
       variant="outline"
       className="
